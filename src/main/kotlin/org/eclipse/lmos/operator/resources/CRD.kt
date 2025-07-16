fd9123267eb9ed630ebe1d400d78f5eb6cf7d17b
Stefan Bußweiler <stefan.bussweiler@telekom.de>
2025-07-16 15:48:26 +0200

    fixtures

--- src/main/kotlin/org/eclipse/lmos/operator/resources/CRD.kt
+++ src/main/kotlin/org/eclipse/lmos/operator/resources/CRD.kt
@@ -21,6 +21,8 @@ import io.fabric8.kubernetes.model.annotation.Version
 import org.eclipse.lmos.operator.resolver.ResolveStrategy
 import org.eclipse.lmos.operator.resolver.Wire
 
+class EmptyStatus
+
 @Group("lmos.eclipse")
 @Version("v2")
 @Plural("agents")
@@ -28,7 +30,17 @@ import org.eclipse.lmos.operator.resolver.Wire
 @Kind("Agent")
 @ShortNames("ag")
 class AgentResource :
-    CustomResource<AgentSpec, Void>(),
+    CustomResource<AgentSpec, EmptyStatus>(),
+    Namespaced
+
+@Group("lmos.eclipse")
+@Version("v1", storage = false)
+@Plural("agents")
+@Singular("agent")
+@Kind("Agent")
+@ShortNames("ag")
+class AgentResourceV1 :
+    CustomResource<AgentSpecV1, EmptyStatus>(),
     Namespaced
 
 data class AgentSpec(
@@ -39,8 +51,20 @@ data class AgentSpec(
     var providedCapabilities: Set<ProvidedCapability> = emptySet(),
 )
 
+data class AgentSpecV1(
+    var description: String = "",
+    var supportedTenants: Set<String> = emptySet(),
+    var supportedChannels: Set<String> = emptySet(),
+    var providedCapabilities: Set<ProvidedCapabilityV1> = emptySet(),
+)
+
 sealed class Capability {
     abstract var id: String
+    abstract var name: String
+    abstract var version: String
+}
+
+sealed class CapabilityV1 {
     abstract var name: String
     abstract var version: String
 }
@@ -60,6 +84,15 @@ data class ProvidedCapability(
     var examples: List<String> = emptyList(),
 ) : Capability()
 
+data class ProvidedCapabilityV1(
+    @JsonPropertyDescription("The name of the capability")
+    @Required
+    override var name: String,
+    @Required
+    override var version: String,
+    var description: String = "",
+) : CapabilityV1()
+
 @Group("lmos.eclipse")
 @Version("v2")
 @Plural("channels")
@@ -68,6 +101,16 @@ data class ProvidedCapability(
 @ShortNames("ch")
 class ChannelResource :
     CustomResource<ChannelSpec, ChannelStatus>(),
+    Namespaced
+
+@Group("lmos.eclipse")
+@Version("v1", storage = false)
+@Plural("channels")
+@Singular("channel")
+@Kind("Channel")
+@ShortNames("ch")
+class ChannelResourceV1 :
+    CustomResource<ChannelSpec, ChannelStatusV1>(),
     Namespaced
 
 enum class ResolveStatus {
@@ -87,16 +130,37 @@ data class RequiredCapability(
     var strategy: ResolveStrategy = ResolveStrategy.HIGHEST,
 ) : Capability()
 
+data class RequiredCapabilityV1(
+    @JsonPropertyDescription("The name of the capability")
+    @Required
+    override var name: String,
+    @Required
+    override var version: String,
+    var strategy: ResolveStrategy = ResolveStrategy.HIGHEST,
+) : CapabilityV1()
+
 data class ChannelStatus(
     @PrinterColumn(name = "RESOLVE_STATUS")
     var resolveStatus: ResolveStatus = ResolveStatus.UNRESOLVED,
     var unresolvedRequiredCapabilities: Set<RequiredCapability> = emptySet(),
 )
 
+data class ChannelStatusV1(
+    @PrinterColumn(name = "RESOLVE_STATUS")
+    var resolveStatus: ResolveStatus = ResolveStatus.UNRESOLVED,
+    var unresolvedRequiredCapabilities: Set<RequiredCapabilityV1> = emptySet(),
+)
+
 @JsonDeserialize
 data class ChannelSpec(
     @Required
     var requiredCapabilities: Set<RequiredCapability>,
+)
+
+@JsonDeserialize
+data class ChannelSpecV1(
+    @Required
+    var requiredCapabilities: Set<RequiredCapabilityV1>,
 )
 
 @Group("lmos.eclipse")
@@ -106,7 +170,17 @@ data class ChannelSpec(
 @Kind("ChannelRollout")
 @ShortNames("crl")
 class ChannelRolloutResource :
-    CustomResource<ChannelRolloutSpec, Void>(),
+    CustomResource<ChannelRolloutSpec, EmptyStatus>(),
+    Namespaced
+
+@Group("lmos.eclipse")
+@Version("v1", storage = false)
+@Plural("channelrollouts")
+@Singular("channelrollout")
+@Kind("ChannelRollout")
+@ShortNames("crl")
+class ChannelRolloutResourceV1 :
+    CustomResource<ChannelRolloutSpec, EmptyStatus>(),
     Namespaced
 
 data class StableChannel(
@@ -139,11 +213,25 @@ data class ChannelRolloutSpec(
 @Kind("ChannelRouting")
 @ShortNames("cr")
 class ChannelRoutingResource :
-    CustomResource<ChannelRoutingSpec, Void>(),
+    CustomResource<ChannelRoutingSpec, EmptyStatus>(),
+    Namespaced
+
+@Group("lmos.eclipse")
+@Version("v1", storage = false)
+@Plural("channelroutings")
+@Singular("channelrouting")
+@Kind("ChannelRouting")
+@ShortNames("cr")
+class ChannelRoutingResourceV1 :
+    CustomResource<ChannelRoutingSpecV1, EmptyStatus>(),
     Namespaced
 
 data class ChannelRoutingSpec(
     var capabilityGroups: Set<CapabilityGroup> = setOf(),
+)
+
+data class ChannelRoutingSpecV1(
+    var capabilityGroups: Set<CapabilityGroupV1> = setOf(),
 )
 
 data class ChannelRoutingStatus(
@@ -162,6 +250,14 @@ data class CapabilityGroup(
     var description: String = "",
 )
 
+data class CapabilityGroupV1(
+    @Required
+    var name: String,
+    @Required
+    var capabilities: Set<ChannelRoutingCapabilityV1>,
+    var description: String = "",
+)
+
 data class ChannelRoutingCapability(
     @Required
     var id: String,
@@ -177,6 +273,26 @@ data class ChannelRoutingCapability(
 ) {
     constructor(wire: Wire<AgentResource>) : this(
         id = wire.providedCapability.id,
+        name = wire.providedCapability.name,
+        requiredVersion = wire.requiredCapability.version,
+        providedVersion = wire.providedCapability.version,
+        description = wire.providedCapability.description,
+        host = "${wire.provider.metadata.name}.${wire.provider.metadata.namespace}.svc.cluster.local",
+    )
+}
+
+data class ChannelRoutingCapabilityV1(
+    @Required
+    var name: String,
+    @Required
+    var requiredVersion: String,
+    @Required
+    var providedVersion: String,
+    @Required
+    var host: String,
+    var description: String = "",
+) {
+    constructor(wire: Wire<AgentResource>) : this(
         name = wire.providedCapability.name,
         requiredVersion = wire.requiredCapability.version,
         providedVersion = wire.providedCapability.version,
