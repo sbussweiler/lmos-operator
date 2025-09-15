b0ae4c02ea5ede49c88b578b2f607c0a6c8d5d40
Patrick Schwager <patrick.schwager@telekom.de>
2025-09-15 11:49:05 +0200

    add prometheus exporter

--- build.gradle.kts
+++ build.gradle.kts
@@ -186,6 +186,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
 
     implementation("io.javaoperatorsdk:operator-framework-spring-boot-starter:$operatorFrameworkVersion")
     implementation("io.fabric8:generator-annotations:7.3.1")
